5384fb6158bc41e549fc12995458fe978df909b8
ZZcat <zach@zettervall.com>
2020-07-31 01:08:43 -0500

    Fix Build errors

--- src/com/aris/challenges/Main.java
+++ src/com/aris/challenges/Main.java
@@ -17,14 +17,13 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 // Import aris scripts
-import com.aris.arismod.ArisCommands;
+
 
 
 public class Main extends JavaPlugin{
 
 	@Override
 	public void onEnable () {
-		getCommand("aris").setExecutor(new ArisCommands());
 		getLogger().info("Aris has been enabled!");
 	}
 	
@@ -33,32 +32,5 @@ public class Main extends JavaPlugin{
 		getLogger().info("Aris is offline ;(");
 	}
 	
-
-	public class CustomChunkGenerator extends ChunkGenerator {
-		int currentHeight = 50;
-
-		@Override
-		public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
-			SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
-			ChunkData chunk = createChunkData(world);
-			generator.setScale(0.005D);
-
-			for (int X = 0; X < 16; X++)
-				for (int Z = 0; Z < 16; Z++) {
-					currentHeight = (int) (generator.noise(chunkX*16+X, chunkZ*16+Z, 0.5D, 0.5D)*15D+50D);
-					chunk.setBlock(X, currentHeight, Z, Material.GRASS);
-					chunk.setBlock(X, currentHeight-1, Z, Material.DIRT);
-					for (int i = currentHeight-2; i > 0; i--)
-						chunk.setBlock(X, i, Z, Material.STONE);
-					chunk.setBlock(X, 0, Z, Material.BEDROCK);
-				}
-			return chunk;
-		}
-	}
-
-
-	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
-	    return new CustomChunkGenerator();
-	}
 	
 }
