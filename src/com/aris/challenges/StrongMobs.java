282f20a6254d0efcd2f863169c071f4be56c5c04
ZZcat <zach@zettervall.com>
2020-08-01 02:27:04 -0500

    Add control option

--- src/com/aris/challenges/StrongMobs.java
+++ src/com/aris/challenges/StrongMobs.java
@@ -7,33 +7,39 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
+
+import com.aris.aris.Main;
+
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 
 public class StrongMobs implements Listener {
 	@EventHandler
 	public void creatureSpawnEvent(CreatureSpawnEvent event) {
-		
-		if (event.getEntityType() == EntityType.CREEPER) {
-			
-			Creeper creeper = (Creeper) event.getEntity();
-			
-			creeper.setPowered(true);
-		}
-		
-		if (event.getEntityType() == EntityType.ZOMBIE) {
-			
-			Zombie zombie = (Zombie) event.getEntity();
-			
-			zombie.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
-			zombie.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
-			zombie.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
-			zombie.getEquipment().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
-			
-			ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
-			sword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
-			
-			zombie.getEquipment().setItemInMainHand(sword);
+
+		if ((boolean) Main.activationHandler.get("StrongMobs")) {
+
+			if (event.getEntityType() == EntityType.CREEPER) {
+
+				Creeper creeper = (Creeper) event.getEntity();
+
+				creeper.setPowered(true);
+			}
+
+			if (event.getEntityType() == EntityType.ZOMBIE) {
+
+				Zombie zombie = (Zombie) event.getEntity();
+
+				zombie.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+				zombie.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+				zombie.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+				zombie.getEquipment().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+
+				ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
+				sword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+
+				zombie.getEquipment().setItemInMainHand(sword);
+			}
 		}
 	}
 }
